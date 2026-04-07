81f85845b2d3633e8dba941e9b8f080de92d3354
UCOB - APLAC - CAP - TI 1 <cap.ti1@adventistas.org>
2026-04-07 11:16:39 -0300

    Fixing main.tf egress to_port

--- infra/main.tf
+++ infra/main.tf
@@ -26,7 +26,7 @@ resource "aws_security_group" "securitygoup" {
 
   egress {
     from_port = 80
-    to_port = 65550
+    to_port = 65535
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
