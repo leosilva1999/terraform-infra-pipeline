f165dc0dae7d077af3916778c2c14077e8a4b9ec
UCOB - APLAC - CAP - TI 1 <cap.ti1@adventistas.org>
2026-04-23 17:03:20 -0300

    using iptables to access api container by the public ip

--- infra/user_data.sh
+++ infra/user_data.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
+
+# ECS config
 echo ECS_CLUSTER=titools-cluster >> /etc/ecs/ecs.config
 
+# Pacotes
 yum update -y
 amazon-linux-extras install nginx1 -y
+
+# NGINX
 systemctl enable nginx
 systemctl start nginx
 
-sudo nano /usr/local/bin/update_nginx_upstream.sh
+
+cat <<'EOF' > /usr/local/bin/update_nginx_upstream.sh
+#!/bin/bash
 
 TASK_IP=$(curl -s http://localhost:51678/v1/tasks \
   | grep -oP '"IPv4Addresses":\s*\["\K[0-9.]+' | head -n 1)
@@ -16,7 +23,7 @@ if [ -z "$TASK_IP" ]; then
   exit 1
 fi
 
-cat <<EOF > /etc/nginx/conf.d/titools.conf
+cat <<EOC > /etc/nginx/conf.d/titools.conf
 server {
     listen 80;
 
@@ -26,15 +33,21 @@ server {
         proxy_set_header X-Real-IP \$remote_addr;
     }
 }
-EOF
+EOC
 
 systemctl reload nginx
+EOF
 
 chmod +x /usr/local/bin/update_nginx_upstream.sh
 
-# espera ECS iniciar tasks
-sleep 60
+
+until curl -s http://localhost:51678/v1/tasks | grep -q IPv4Addresses; do
+  echo "Aguardando task subir..."
+  sleep 5
+done
+
 
 /usr/local/bin/update_nginx_upstream.sh
+
 
 echo "*/1 * * * * /usr/local/bin/update_nginx_upstream.sh" | crontab -
